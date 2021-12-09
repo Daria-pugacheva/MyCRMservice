b460248de946ca46b008dfc57106c667b9b39bab
geekbrainsPugachevaD <dpugachev@uandex.ru>
2021-12-09 23:01:44 +0300

    Initial commit

---
+++ src/test/java/ru/gb/pugacheva/crm/myCRMservice/MyCrMserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.pugacheva.crm.myCRMservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrMserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
